30f79358cff5ea8b1d225447f7623df34c065cb0
christophevg-agent <contact+agent@christophe.vg>
2026-06-06 20:49:22 +0200

    add example of mixing globals and commands

--- examples/commands.py
+++ examples/commands.py
@@ -8,24 +8,40 @@ Run with:
     uv run python commands.py chk --verbose    # Using alias 'chk' for 'check'
 """
 
+from dataclasses import dataclass
 from rich.pretty import pprint
 
 from clevis import configclass, get_cmd, get_config, SecurityAction
 
+@configclass
+class Globals:
+  directory : str = "."
 
-@configclass(cmd="check", help="Run diagnostics", aliases=["c", "chk"])
-class CheckConfig:
-  verbose: bool = False
-
+@dataclass
+class BaseConfig:
+  parameter : str = None
 
 @configclass(cmd="print", help="Print configuration", aliases=["p"])
 class PrintConfig:
   rich: bool = False
 
+@configclass(cmd="check", help="Run diagnostics", aliases=["c", "chk"])
+class CheckConfig(BaseConfig):
+  verbose: bool = False
+
+@configclass(cmd="echo")
+class EchoConfig(BaseConfig):
+  pass
 
 if __name__ == "__main__":
+  security = {
+    "file_permissions": SecurityAction.LOG,
+    "directory_permissions": SecurityAction.LOG
+  }
   cmd = get_cmd()
-  security = {"file_permissions": SecurityAction.LOG, "directory_permissions": SecurityAction.LOG}
+  config = get_config(Globals)
+  if config.directory != ".":
+    print(f"changing to directory {config.directory}")
   if cmd == "check":
     config = get_config(CheckConfig, project=False, user=False, security=security)
     print(f"checking verbose={config.verbose}")
@@ -35,4 +51,6 @@ if __name__ == "__main__":
       pprint(config)
     else:
       print(config)
-
+  elif cmd == "echo":
+    config = get_config(EchoConfig, project=False, user=False, security=security)
+    print(config.parameter)
